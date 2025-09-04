9ffbf06b17b92e4da70b57cd102b7c8113db442a
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2025-09-04 10:23:09 +0600

    Add asset generation and color definitions using FlutterGen

---
+++ lib/gen/colors.gen.dart
@@ -0,0 +1,43 @@
+// dart format width=80
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  /// Color: #FFFFB300
+  static const Color allPrimaryColor = Color(0xFFFFB300);
+
+  /// Color: #FF000000
+  static const Color blackColor = Color(0xFF000000);
+
+  /// Color: #FF616161
+  static const Color darkGreyColor = Color(0xFF616161);
+
+  /// Color: #FF9E9E9E
+  static const Color greyColor = Color(0xFF9E9E9E);
+
+  /// Color: #FFF5F5F5
+  static const Color lightGreyColor = Color(0xFFF5F5F5);
+
+  /// Color: #FF1A1A1A
+  static const Color primaryDark = Color(0xFF1A1A1A);
+
+  /// Color: #FFf9efef
+  static const Color scaffoldColor = Color(0xFFF9EFEF);
+
+  /// Color: #FFFF3B3B
+  static const Color secondaryColor = Color(0xFFFF3B3B);
+
+  /// Color: #FFFFFFFF
+  static const Color whiteColor = Color(0xFFFFFFFF);
+}
