d20308847f89ab213aadfa634fa8f1c22d090e70
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2025-09-11 03:20:12 +0600

    chore: Replace hardcoded Google Maps API key with placeholder in configuration files

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDZn0SMbuKa3vm5cKJ4FCms0zIkgPMvuRE")
+    GMSServices.provideAPIKey("YOUR_API_KEY_HERE")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
